e0bb18c798ebf8baff74f9fd9f4144fdad4efcbb
edna-kzn <77274244+edna-kzn@users.noreply.github.com>
2021-02-21 14:51:48 +0330

    search emage button is working

--- app/src/main/java/com/kazarian/android/findmoviesapplication/FilmRecyclerAdapter.java
+++ app/src/main/java/com/kazarian/android/findmoviesapplication/FilmRecyclerAdapter.java
@@ -7,7 +7,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 public class FilmRecyclerAdapter extends RecyclerView.Adapter<FilmViewHolder> {
+
+    ArrayList<String> fname, fjenre, fyear, fimage;
+    ArrayList<Double> fimdbid;
+
+
+    public FilmRecyclerAdapter(ArrayList<String> fnames, ArrayList<String> fyears, ArrayList<String> fjenres, ArrayList<String> fimages){
+        this.fname = fnames;
+        this.fyear = fyears;
+        this.fjenre = fjenres;
+        this.fimage = fimages;
+
+    }
+
     @NonNull
     @Override
     public FilmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -19,11 +36,19 @@ public class FilmRecyclerAdapter extends RecyclerView.Adapter<FilmViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull FilmViewHolder holder, int position) {
         //vaghti data ra az internet khandim va dar list gozashtim bad in ghesmat neveshte shavad
+        holder.txtfname.setText("Title: " + fname.get(position).toString());
+        holder.txtfgenre.setText("Type: " + fjenre.get(position).toString());
+        holder.txtFYear.setText("Year: " + fyear.get(position).toString());
+        Picasso.get().load(fimage.get(position)).fit().into(holder.imgfimage);
+
     }
 
     @Override
     public int getItemCount() {
-        //tedad film haye peyda shode dar search ya darvaghe saze listi ke natije search name film por shode
-        return 0;
+        //tedad film haye peyda shode dar search ya darvaghe saze listi ke az natije search name film por shode
+        int i = 0;
+        if (fname != null)
+            i = fname.size();
+        return i;
     }
 }
--- app/src/main/java/com/kazarian/android/findmoviesapplication/MainActivity.java
+++ app/src/main/java/com/kazarian/android/findmoviesapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.kazarian.android.findmoviesapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
@@ -14,20 +19,101 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import Omdbapi_Movie_info_model.OmdbapiMovieInfoModel;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> fnames, fjenres, fyears, fimages;
+    ArrayList<Double> fimdbids;
+
+    //Title (in OmdbapiMovieInfoModel) = FName
+    //Year (in OmdbapiMovieInfoModel) = FYear
+    //ImdbID (in OmdbapiMovieInfoModel) = FImdbID
+    //Type (in OmdbapiMovieInfoModel) = FGenre
+    //Poster (in OmdbapiMovieInfoModel) = FImage
+
+    String myUrl = "http://www.omdbapi.com/?s=current&apikey=5e5354bb";
+    String changablefase = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //-------connecting to site omdb api-------start---------------
+        ImageView imgSearch = findViewById(R.id.imgSearch);
+        EditText edtSearch = findViewById(R.id.edtSearch);
 
-        String myUrl = "http://www.omdbapi.com/?t=gun&apikey=5e5354bb";   //<----------- url omdb api bayad bashad
+
+        //-------connecting to site omdb api-------start---------------
+        //<----------- url omdb api bayad bashad
         AsyncHttpClient client = new AsyncHttpClient();
+
+        //------making the url string for connecting to omdb site ------
+        imgSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myUrl = "http://www.omdbapi.com/?s=";
+                String changablefase = edtSearch.getText().toString();   //using this variable to make myUrl complete with adding edtSearch
+
+                if (changablefase != "") {
+                    myUrl = myUrl.toString() + changablefase.toString() + "&apikey=5e5354bb";
+
+                    //------receiving json code------------------------------------
+                    client.get(myUrl, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            super.onSuccess(statusCode, headers, response);
+                            //-----mapping json code to Gson class----------------
+                            Gson gson = new Gson();
+                            OmdbapiMovieInfoModel model = gson.fromJson(response.toString(), OmdbapiMovieInfoModel.class);
+                            //-----extracting data from Gson class----------------
+
+                            fnames = new ArrayList<>();
+                            fyears = new ArrayList<>();
+                            //fimdbids = new ArrayList<>();
+                            fjenres = new ArrayList<>();
+                            fimages = new ArrayList<>();
+
+                            for (int i = 0; i< model.getSearch().size(); i++){
+                                fnames.add(model.getSearch().get(i).getTitle());
+                                fyears.add(model.getSearch().get(i).getYear());
+                                //fimdbids.add(model.getSearch().get(i).getImdbID());
+                                fjenres.add(model.getSearch().get(i).getType());
+                                fimages.add(model.getSearch().get(i).getPoster());
+                            }
+
+                            System.out.println(response.toString());
+
+                            //-----preparing to show data in recycler View-----------
+                            RecyclerView myRecycler = findViewById(R.id.recycler);
+
+                            LinearLayoutManager myManager = new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false);
+                            myRecycler.setLayoutManager(myManager);
+
+                            //i shude make an adapter for recycler view here
+                            FilmRecyclerAdapter adapter = new FilmRecyclerAdapter(fnames, fyears, fjenres, fimages);
+                            myRecycler.setAdapter(adapter);
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            super.onFailure(statusCode, headers, throwable, errorResponse);
+                        }
+                    });
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Please Enter Keyword!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+
         //------receiving json code------------------------------------
         client.get(myUrl, new JsonHttpResponseHandler() {
             @Override
@@ -37,10 +123,20 @@ public class MainActivity extends AppCompatActivity {
                 Gson gson = new Gson();
                 OmdbapiMovieInfoModel model = gson.fromJson(response.toString(), OmdbapiMovieInfoModel.class);
                 //-----extracting data from Gson class----------------
-                String fname = model.getTitle();
-                String fjenre = model.getGenre();
-                String fyear = model.getYear();
-                String fimage = model.getPoster();
+
+                fnames = new ArrayList<>();
+                fyears = new ArrayList<>();
+                //fimdbids = new ArrayList<>();
+                fjenres = new ArrayList<>();
+                fimages = new ArrayList<>();
+
+                for (int i = 0; i< model.getSearch().size(); i++){
+                    fnames.add(model.getSearch().get(i).getTitle());
+                    fyears.add(model.getSearch().get(i).getYear());
+                    //fimdbids.add(model.getSearch().get(i).getImdbID());
+                    fjenres.add(model.getSearch().get(i).getType());
+                    fimages.add(model.getSearch().get(i).getPoster());
+                }
 
                 System.out.println(response.toString());
 
@@ -51,14 +147,19 @@ public class MainActivity extends AppCompatActivity {
                 myRecycler.setLayoutManager(myManager);
 
                 //i shude make an adapter for recycler view here
+                FilmRecyclerAdapter adapter = new FilmRecyclerAdapter(fnames, fyears, fjenres, fimages);
+                myRecycler.setAdapter(adapter);
+
+
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
             }
-        });
 
+
+        });
 
 
 
